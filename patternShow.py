97526e9d20808bdfbf21c94e156253e66ca3b70a
Chris Brinson <chrisbrinson@pop-os.localdomain>
2019-03-05 21:42:51 +0100

    added bkgd detection

--- patternShow.py
+++ patternShow.py
@@ -1,15 +1,23 @@
 import os
+import numpy as np
+import cv2 as cv
 
 def showPatterns():
     directory = 'patterns'
+    cap = cv.VideoCapture(0)
+    fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
     for filename in os.listdir(directory):
-        print(os.path.join(directory, filename))
+        # print(os.path.join(directory, filename))
         with open(os.path.join(directory, filename)) as f:
             lines = f.readlines()
-            print(lines)
-            #TODO show leds for each line
-            #TODO run background check at the same time
-            #TODO take picture if white balance hits over certain amount.
+            for line in lines:
+                leds = line.split(",")
+                #TODO show leds for each line
+
+                ret, frame = cap.read()
+                fgmask = fgbg.apply(frame)
+                cv.imshow('frame',fgmask)
+                #TODO take picture if white balance hits over certain amount.
 
 if __name__ == "__main__":
     showPatterns()
