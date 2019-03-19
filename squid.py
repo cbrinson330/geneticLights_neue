fdcf3112d90370ee0527cb4cf2d4a21bdc9f024a
Chris Brinson <chrisbrinson@pop-os.localdomain>
2019-03-19 07:11:14 +0100

    added call to find faces

--- squid.py
+++ squid.py
@@ -1,6 +1,6 @@
 from patternShow import runPatterns
+from detectFaces import detectFaces
 #import population
-#import detecFaces
 
 if __name__ == "__main__":
     #Dir paths
@@ -11,18 +11,19 @@ if __name__ == "__main__":
     displayWidth = 32
     displayHeight = 9
     timesToRepeatAllPatterns = 2
-    #Num of secs between each line in a pattern
-    stepLength = 0.5
+    stepLength = 0.5 #Num of secs between each line in a pattern
 
-
-
+    #Patterns
+    numberOfPatterns = 60
 
     while True:
         rp = runPatterns(displayWidth, displayHeight, patternStorageDir, imgStorageDir, timesToRepeatAllPatterns, stepLength)
         rp.showPatterns()
 
-        #TODO show patterns
+        #TODO calculate faces seen
+        df = detectFaces(imgStorageDir, numberOfPatterns)
+        df.detectFaceForImage()
+
+        #TODO order patterns based on number of faces seen
         #TODO pop = Population(size=60, crossover=0.8, elitism=0.1, mutation=0.3)
         #TODO pop.evolve()
-        #TODO calculate faces seen
-        #TODO order patterns based on number of faces seen
